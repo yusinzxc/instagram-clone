7e4535d1292f4dd199078e4d8e95db2081d44313
Oddeye <yusinzxc>
2021-12-23 20:27:18 +0800

    first commit

---
+++ create-post.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=7">
+        <title>Instagram - Create post</title>
+        <link rel="stylesheet" href="css/home.css" media="all">
+    </head>
+    <body>
+        <div class="main-home">
+            <div class="home-navbar">
+                <div class="ig-logo">
+                    <img src="icons\instagram-text.svg" alt="">
+                </div>
+                <div class="service-icon">
+                    <a href="home.php">
+                    <img src="icons\home-outline.svg" name="home-icon">
+                    </a>
+                    <a href="search.php">
+                    <img src="icons\search.svg" name="search-icon" style="height: 24.5px;">
+                    </a>
+                    <a href="explore.php">
+                    <img src="icons\explore-outline.svg" name="explore-icon">
+                    </a>
+                    <a href="notification.php">
+                    <img src="icons\heart-outline.svg" name="heart-icon">
+                    </a>
+                </div>
+                <div class="profile">
+                    <a href="inbox.php">
+                    <img src="icons\messenger-outline.svg" name="messenger-icon">
+                    </a>
+                    <a href="profile.php">
+                        <div class="user-pic">
+                            <img src="" alt="">
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="content"></div>
+            <div class="responsive-service-icon">
+                <a href="home.php">
+                <img src="icons\home-outline.svg" name="home-icon">
+                </a>
+                <a href="search.php">
+                <img src="icons\search.svg" name="search-icon">
+                </a>
+                <a href="create-post.php">
+                <img src="icons\plus-solid.svg" name="explore-icon">
+                </a>
+                <a href="explore.php">
+                <img src="icons\explore-outline.svg" name="explore-icon">
+                </a>
+                <a href="notification.php">
+                <img src="icons\heart-outline.svg" name="heart-icon">
+                </a>
+            </div>
+        </div>
+    </body>
+</html>
